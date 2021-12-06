759b1f5ddd38dbb480d4aed92b55d3eaccbc55c6
Narfinger <narfinger@noreply.github.com>
2021-12-06 15:44:06 +0900

    some stuff

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,23 @@ use anyhow::Result;
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    moves: Vec<Move>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 struct Move {
     pattern: String,
     path: String,
 }
 
 fn main() -> Result<()> {
-    let config: Vec<Move> = {
+    let config: Config = {
         let config_file = read_to_string("move.toml")?;
         toml::from_str(&config_file)?
     };
+
+    println!("{:?}", config);
 
     let cur_dir = std::env::current_dir()?;
     let entries = fs::read_dir(cur_dir)?
@@ -27,9 +34,9 @@ fn main() -> Result<()> {
     for i in entries
         .iter()
         .filter(|p| !p.is_dir())
-        .filter(|p| p.extension().unwrap() == "mkv")
+        .filter(|p| p.extension().map_or(false, |f| f == "mkv"))
     {
-        for m in config.iter() {
+        for m in config.moves.iter() {
             let filename = i.file_name().unwrap().to_str().unwrap().to_owned();
             if filename.contains(&m.pattern) {
                 println!("Matching {} with {}", &filename, &m.pattern);
